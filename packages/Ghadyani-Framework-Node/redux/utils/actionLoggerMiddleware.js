62a8385b8ae81dd34d771cf245a5558d9f7f2672
Kevin Ghadyani <Saturn2888@gmail.com>
2018-07-22 23:17:43 -0500

    Added log colors to actionLoggerMiddleware

--- packages/Ghadyani-Framework-Node/redux/utils/actionLoggerMiddleware.js
+++ packages/Ghadyani-Framework-Node/redux/utils/actionLoggerMiddleware.js
@@ -1,8 +1,41 @@
+const chalk = require('chalk')
+
 const actionLoggerMiddleware = (
 	() => next => action => {
+		const { type } = action
+		const [
+			,
+			actionTypeGroup,
+			delimiter,
+			actionType,
+		] = (
+			type
+			.match(/^(.+)(::)(.+)$/)
+		)
+
+		const title = (
+			chalk
+			.cyan
+			.bgBlue
+			.bold('[Action]')
+		)
+
+		const message = (
+			chalk
+			.grey(actionTypeGroup)
+			.concat(
+				chalk
+				.green(delimiter)
+			)
+			.concat(
+				chalk
+				.magentaBright(actionType)
+			)
+		)
+
 		console.info(
-			'Action:',
-			action.type,
+			title,
+			message,
 		)
 
 		return next(action)
