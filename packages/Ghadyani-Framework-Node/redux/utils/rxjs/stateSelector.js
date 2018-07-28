ea1b5f7fbb1e8f899141477ac2a96f22e00c8a06
Kevin Ghadyani <Saturn2888@gmail.com>
2018-07-27 20:35:26 -0500

    Added error handling to stateSelector

--- packages/Ghadyani-Framework-Node/redux/utils/rxjs/stateSelector.js
+++ packages/Ghadyani-Framework-Node/redux/utils/rxjs/stateSelector.js
@@ -1,16 +1,35 @@
-const { of } = require('rxjs')
+const chalk = require('chalk')
+const { NEVER, of } = require('rxjs')
 
 const stateSelector = ({
+	errorCallback = () => {},
 	props,
 	selector,
 	state$,
-}) => (
-	of(
-		selector(
-			state$.value,
-			props,
+}) => {
+	try {
+		return (
+			of(
+				selector(
+					state$.value,
+					props,
+				)
+			)
 		)
-	)
-)
+	}
+	catch(exception) {
+		console
+		.error(
+			chalk
+			.red(
+				exception
+			)
+		)
+
+		errorCallback(exception)
+
+		return NEVER
+	}
+}
 
 module.exports = stateSelector
