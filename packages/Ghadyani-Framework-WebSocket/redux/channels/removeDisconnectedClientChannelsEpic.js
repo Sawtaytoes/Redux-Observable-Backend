ac52278bb33247085372b56f7953dc8b0b300672
Kevin Ghadyani <Saturn2888@gmail.com>
2018-07-27 20:36:46 -0500

    Minor formatting in removeDisconnectedClientChannelsEpic

--- packages/Ghadyani-Framework-WebSocket/redux/channels/removeDisconnectedClientChannelsEpic.js
+++ packages/Ghadyani-Framework-WebSocket/redux/channels/removeDisconnectedClientChannelsEpic.js
@@ -1,5 +1,4 @@
 const { map, mergeMap, tap } = require('rxjs/operators')
-const { of } = require('rxjs')
 const { ofType } = require('redux-observable')
 
 const stateSelector = require('@ghadyani-framework/node/redux/utils/rxjs/stateSelector')
@@ -7,25 +6,26 @@ const { getChannelsList } = require('./selectors')
 const { leaveChannel } = require('$redux/channels/actions')
 const { REMOVE_CLIENT } = require('$redux/clients/actions')
 
-const removeDisconnectedClientChannelsEpic = (action$, state$) => (
-	action$
-	.pipe(
-		ofType(REMOVE_CLIENT),
-		mergeMap(({ connection }) => (
-			stateSelector({
-				selector: getChannelsList,
-				state$,
-			})
-			.pipe(
-				map(channelName => (
-					leaveChannel({
-						connection,
-						namespace: channelName,
-					})
-				)),
-				map(of),
-			)
-		)),
+const removeDisconnectedClientChannelsEpic = (
+	(action$, state$) => (
+		action$
+		.pipe(
+			ofType(REMOVE_CLIENT),
+			mergeMap(({ connection }) => (
+				stateSelector({
+					selector: getChannelsList,
+					state$,
+				})
+				.pipe(
+					map(channelName => (
+						leaveChannel({
+							connection,
+							namespace: channelName,
+						})
+					)),
+				)
+			)),
+		)
 	)
 )
 
