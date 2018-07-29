f8e362dfacd8bb7289eaac7fa41ca15a599758c4
Kevin Ghadyani <Saturn2888@gmail.com>
2018-07-29 02:56:43 -0500

    Added missing helper functions to index.js

--- packages/Ghadyani-Framework-Redux-Utils/index.js
+++ packages/Ghadyani-Framework-Redux-Utils/index.js
@@ -3,4 +3,9 @@ require('@ghadyani-framework/setup-module-aliases')(__dirname)
 
 module.exports = {
 	createActionLoggerMiddleware: require('$utils/createActionLoggerMiddleware'),
+	createReducer: require('$utils/createReducer'),
+	mapToState: require('$utils/mapToState'),
+	namespaceReducer: require('$utils/namespaceReducer'),
+	simpleMap: require('$utils/simpleMap'),
+	stateSelector: require('$utils/stateSelector'),
 }
