a62685f69a1c79f40a7fd6381325df0db07a100d
Kevin Ghadyani <Saturn2888@gmail.com>
2018-07-29 00:25:53 -0500

    Moved createStore to app.js
    Made it so

--- packages/Ghadyani-Framework-Node/app.js
+++ packages/Ghadyani-Framework-Node/app.js
@@ -1,13 +1,46 @@
+const { applyMiddleware, createStore } = require('redux')
+const { createEpicMiddleware } = require('redux-observable')
 const { of } = require('rxjs')
 const { tap } = require('rxjs/operators')
 
+// Include this import before other local imports.
 const {
 	createConfigurationSet,
 	createReduxStore,
 	runTasks,
 } = require('./')
 
-of(createReduxStore({}))
+const createActionLoggerMiddleware = require('$redux/utils/createActionLoggerMiddleware')
+
+const {
+	rootEpic,
+	rootReducer,
+} = require('$redux')
+
+const actionLoggerMiddleware = (
+	createActionLoggerMiddleware()
+)
+
+const epicMiddleware = createEpicMiddleware()
+
+const middleware = (
+	applyMiddleware(
+		actionLoggerMiddleware,
+		epicMiddleware
+	)
+)
+
+const store = (
+	createStore(
+		rootReducer,
+		middleware,
+	)
+)
+
+epicMiddleware
+.run(rootEpic)
+
+of(store)
 .pipe(
 	tap(createConfigurationSet({})),
 	tap(runTasks('eslint')),
