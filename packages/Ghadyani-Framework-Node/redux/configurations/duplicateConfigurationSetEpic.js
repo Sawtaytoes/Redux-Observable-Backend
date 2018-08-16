c22955cf366216fd79177c444c837c97f8496c1b
Kevin Ghadyani <Saturn2888@gmail.com>
2018-08-15 22:11:51 -0500

    Minor formatting in duplicateConfigurationSetEpic

--- packages/Ghadyani-Framework-Node/redux/configurations/duplicateConfigurationSetEpic.js
+++ packages/Ghadyani-Framework-Node/redux/configurations/duplicateConfigurationSetEpic.js
@@ -1,8 +1,8 @@
 const { map, switchMap } = require('rxjs/operators')
 const { ofType } = require('redux-observable')
+const { stateSelector } = require('@ghadyani-framework/redux-utils')
 
 const { configurationSetSelector } = require('./selectors')
-const { stateSelector } = require('@ghadyani-framework/redux-utils')
 
 const {
 	addConfigurationSet,
