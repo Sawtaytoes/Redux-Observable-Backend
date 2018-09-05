32661d4f2fecd401495b6384ba53cfd8ab4e9c2b
Kevin Ghadyani <Saturn2888@gmail.com>
2018-09-04 22:52:02 -0500

    Moved deprecated methods to index.js

--- packages/Ghadyani-Framework-Redux-Utils/index.js
+++ packages/Ghadyani-Framework-Redux-Utils/index.js
@@ -1,8 +1,27 @@
 #!/usr/bin/env node
+
+// This import must come before setting up module aliases.
+// Because `base` brings in `setup-module-aliases`, it needs to run before setting the alias when linking packages.
+const { createDeprecatedFunction } = require('@ghadyani-framework/base')
+
 require('@ghadyani-framework/setup-module-aliases')(__dirname)
 
 module.exports = {
-	...require('$utils/deprecated'),
+	namespaceReducer: (
+		createDeprecatedFunction({
+			deprecatedMethodName: 'namespaceReducer',
+			func: require('$utils/createNamespaceReducer'),
+			replacementMethodName: 'createNamespaceReducer',
+		})
+	),
+	reduceReducer: (
+		createDeprecatedFunction({
+			deprecatedMethodName: 'reduceReducer',
+			func: require('$utils/createReduceReducers'),
+			replacementMethodName: 'createReduceReducers',
+		})
+	),
+
 	createActionLoggerMiddleware: require('$utils/createActionLoggerMiddleware'),
 	createMergeById: require('$utils/createMergeById'),
 	createNamespaceReducer: require('$utils/createNamespaceReducer'),
