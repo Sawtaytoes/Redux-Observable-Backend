6a3a3210a1513b9a22a7571747c422d3dc5d5f8e
Kevin Ghadyani <Saturn2888@gmail.com>
2018-10-07 03:47:45 -0500

    Removed memorization from createNamespaceSelector

--- packages/Ghadyani-Framework-Redux-Utils/utils/createNamespacedSelector.js
+++ packages/Ghadyani-Framework-Redux-Utils/utils/createNamespacedSelector.js
@@ -1,30 +1,12 @@
 const createNamespaceSelector = (
-	(select, format) => {
-		const previousSelectedValue = {}
-		const previousFormattedValue = {}
-
-		return (
-			(state, props) => {
-				const selectedValue = select(state, props)
-				const { namespace } = props
-
-				const formattedValue = (
-					selectedValue !== previousSelectedValue[namespace]
-					? ({
-						[namespace]: (
-							format(selectedValue)
-						),
-					})
-					: previousFormattedValue[namespace]
-				)
-
-				previousSelectedValue[namespace] = selectedValue
-				previousFormattedValue[namespace] = formattedValue
-
-				return formattedValue
-			}
-		)
-	}
-)
+	select,
+) => (
+	state,
+	props,
+) => ({
+	[props.namespace]: (
+		select(state, props)
+	),
+})
 
 module.exports = createNamespaceSelector
