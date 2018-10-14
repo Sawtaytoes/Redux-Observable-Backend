0cf6dee8221a267b004b649708379397875f9c0a
Kevin Ghadyani <Saturn2888@gmail.com>
2018-10-14 05:17:35 -0500

    Minor formatting in WebSocket observable creators

--- packages/Ghadyani-Framework-WebSocket/redux/webSocketServers/utils/createWebSocketConnectionObservable.js
+++ packages/Ghadyani-Framework-WebSocket/redux/webSocketServers/utils/createWebSocketConnectionObservable.js
@@ -3,36 +3,40 @@ const { Observable } = require('rxjs')
 const getQueryParamsFromRequest = require('$utils/getQueryParamsFromRequest')
 
 const createWebSocketConnectionObserver = (
-	webSocketServer => (
-		observer => {
-			webSocketServer
-			.on(
-				'connection',
-				(connection, req) => {
-					const queryParams = (
-						getQueryParamsFromRequest(req)
-					)
-
-					// Necessary mutation as `ws` doesn't give access to `req` in `connection`
-					connection
-					.queryParams = queryParams
-
-					observer
-					.next({
-						connection,
-						queryParams,
-						server: webSocketServer,
-					})
-				}
+	webSocketServer,
+) => (
+	observer,
+) => {
+	webSocketServer
+	.on(
+		'connection',
+		(connection, req) => {
+			const queryParams = (
+				getQueryParamsFromRequest(req)
 			)
+
+			// Necessary mutation as `ws` doesn't give access to `req` in `connection`
+			connection
+			.queryParams = queryParams
+
+			observer
+			.next({
+				connection,
+				queryParams,
+				server: webSocketServer,
+			})
 		}
 	)
-)
+}
 
-const createWebSocketConnectionObservable = webSocketServer => (
+const createWebSocketConnectionObservable = (
+	webSocketServer,
+) => (
 	Observable
 	.create(
-		createWebSocketConnectionObserver(webSocketServer)
+		createWebSocketConnectionObserver(
+			webSocketServer
+		)
 	)
 )
 
--- packages/Ghadyani-Framework-WebSocket/redux/webSocketServers/utils/createWebSocketMessageObservable.js
+++ packages/Ghadyani-Framework-WebSocket/redux/webSocketServers/utils/createWebSocketMessageObservable.js
@@ -1,45 +1,63 @@
 const { Observable } = require('rxjs')
 
-const createWebSocketMessageObserver = (
-	({ connection, ...props }) => (
-		observer => {
-			connection
-			.on(
-				'message',
-				message => {
-					try {
-						observer.next({
-							...props,
-							connection,
-							message: JSON.parse(message),
-						})
-					} catch (error) {
-						console.info('Received non-JSON message:', message)
-						connection.terminate()
-					}
-				}
-			)
-			.on(
-				'close',
-				closingMessage => (
-					observer.next({
-						isClosed: true,
-						closingMessage,
+const createWebSocketMessageObserver = ({
+	connection,
+	...props
+}) => (
+	observer => {
+		connection
+		.on(
+			'message',
+			message => {
+				try {
+					observer
+					.next({
+						...props,
 						connection,
+						message: JSON.parse(message),
 					})
-				)
+				}
+				catch (error) {
+					console
+					.info(
+						'Received non-JSON message:',
+						message,
+					)
+
+					connection
+					.terminate()
+				}
+			}
+		)
+		.on(
+			'close',
+			closingMessage => (
+				observer
+				.next({
+					isClosed: true,
+					closingMessage,
+					connection,
+				})
 			)
-			.on(
-				'close',
-				() => observer.complete()
-			)
-		}
-	)
+		)
+		.on(
+			'close',
+			() => {
+				observer
+				.complete()
+			}
+		)
+	}
 )
 
-const createWebSocketMessageObservable = webSocket => (
-	Observable.create(
-		createWebSocketMessageObserver(webSocket)
+const createWebSocketMessageObservable = (
+	webSocket,
+) => (
+	Observable
+	.create(
+		createWebSocketMessageObserver(
+			webSocket
+		)
 	)
 )
 
