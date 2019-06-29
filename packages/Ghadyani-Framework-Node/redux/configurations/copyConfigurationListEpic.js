04787c349a082d6d5e0a385699be2cfa875bf603
Kevin Ghadyani <Saturn2888@gmail.com>
2019-06-28 21:24:09 -0500

    Updated configurationSetSelector selector

--- packages/Ghadyani-Framework-Node/redux/configurations/copyConfigurationListEpic.js
+++ packages/Ghadyani-Framework-Node/redux/configurations/copyConfigurationListEpic.js
@@ -3,7 +3,6 @@ const { of } = require('rxjs')
 const { ofType } = require('redux-observable')
 
 const { configurationSetSelector } = require('./selectors')
-const { stateSelector } = require('@ghadyani-framework/redux-utils')
 
 const {
 	addConfigurationSet,
@@ -11,48 +10,51 @@ const {
 } = require('./actions')
 
 const copyConfigurationListEpic = (
-	(action$, state$) => (
-		action$
-		.pipe(
-			ofType(COPY_FROM_CONFIGURATION_SET),
-			switchMap(({
-				configurationCopyList,
-				configurationSetName,
-				namespace,
-			}) => (
-				stateSelector({
-					props: { namespace: configurationSetName },
-					selector: configurationSetSelector,
-					state$,
-				})
-				.pipe(
-					switchMap(configurationSet => (
-						of(configurationSet)
-						.pipe(
-							switchMap(Object.keys),
-							filter(key => (
-								configurationCopyList
-								.includes(key)
-							)),
-							reduce(
-								(combined, key) => ({
-									...combined,
-									[key]: (
-										configurationSet[key]
-									),
-								}),
-								{},
-							),
-						)
-					)),
-					map(configurationSet => ({
-						configurationSet,
-						namespace,
-					})),
-				)
-			)),
-			map(addConfigurationSet),
-		)
+	action$,
+	state$,
+) => (
+	action$
+	.pipe(
+		ofType(COPY_FROM_CONFIGURATION_SET),
+		switchMap(({
+			configurationCopyList,
+			configurationSetName,
+			namespace,
+		}) => (
+			of(state$.value)
+			.pipe(
+				map(
+					configurationSetSelector({
+						namespace: configurationSetName,
+					})
+				),
+				switchMap(configurationSet => (
+					of(configurationSet)
+					.pipe(
+						switchMap(Object.keys),
+						filter(key => (
+							configurationCopyList
+							.includes(key)
+						)),
+						reduce(
+							(
+								combined,
+								key,
+							) => ({
+								...combined,
+								[key]: configurationSet[key],
+							}),
+							{},
+						),
+					)
+				)),
+				map(configurationSet => ({
+					configurationSet,
+					namespace,
+				})),
+			)
+		)),
+		map(addConfigurationSet),
 	)
 )
 
--- packages/Ghadyani-Framework-Node/redux/configurations/selectors.js
+++ packages/Ghadyani-Framework-Node/redux/configurations/selectors.js
@@ -1,25 +1,19 @@
-const { createDeprecatedFunction } = require('@ghadyani-framework/base')
 const { defaultConfigurationsNamespace } = require('$redux/configurations/actions')
 
 const configurationSetSelector = (
-	({ configurations }, props) => (
-		configurations
-		.configurationSets
-		.get(
-			props
-			? props.namespace
-			: defaultConfigurationsNamespace
-		)
+	props,
+) => ({
+	configurations,
+}) => (
+	configurations
+	.configurationSets
+	.get(
+		props
+		? props.namespace
+		: defaultConfigurationsNamespace
 	)
 )
 
 module.exports = {
 	configurationSetSelector,
-	getConfigurationSet: (
-		createDeprecatedFunction({
-			deprecatedMethodName: 'getConfigurationSet',
-			func: configurationSetSelector,
-			replacementMethodName: 'configurationSetSelector',
-		})
-	),
 }
