7a11ca73ab34b190356ba56669f4106af9aec325
Kevin Ghadyani <Saturn2888@gmail.com>
2019-10-11 17:18:30 -0500

    Code styling in runEslint

--- packages/node/redux/tasks/utils/runEslint.js
+++ packages/node/redux/tasks/utils/runEslint.js
@@ -1,37 +1,36 @@
 const { CLIEngine } = require('eslint')
 
 const runEslint = (
-	config => {
-		const cli = (
-			new CLIEngine({
-				cache: true,
-				fix: config.isLocalDevelopment,
-			})
-		)
+	config,
+) => {
+	const cli = (
+		new CLIEngine({
+			cache: true,
+			fix: config.isLocalDevelopment,
+		})
+	)
 
-		const report = (
-			cli
-			.executeOnFiles([
-				'**/*.js',
-			])
-		)
+	const report = (
+		cli
+		.executeOnFiles([
+			'**/*.js',
+		])
+	)
 
-		CLIEngine
-		.outputFixes(report)
+	CLIEngine
+	.outputFixes(report)
 
-		const { errorCount, results } = report
+	const { errorCount, results } = report
 
-		const formatter = cli.getFormatter()
+	const formatter = cli.getFormatter()
 
-		const formattedResults = formatter(results)
+	const formattedResults = formatter(results)
 
-		formattedResults
-		&& console.info(formattedResults)
+	formattedResults
+	&& console.info(formattedResults)
 
-		errorCount > 0
-		&& process.exit()
-	}
-
-)
+	errorCount > 0
+	&& process.exit()
+}
 
 module.exports = runEslint
