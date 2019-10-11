20e1298eb1fb55d27b3733d477fed60fa0b88d49
Kevin Ghadyani <Saturn2888@gmail.com>
2019-10-11 16:33:53 -0500

    Code styling in createDeprecatedFunction

--- packages/core/utils/createDeprecatedFunction.js
+++ packages/core/utils/createDeprecatedFunction.js
@@ -5,21 +5,21 @@ const createDeprecatedFunction = ({
 	func,
 	replacementMethodName,
 }) => (
-	(...args) => {
-		logDeprecation(
-			replacementMethodName
-			? (
-				`\`${deprecatedMethodName}\` is deprecated.`
-				.concat(' ')
-				.concat(`Use \`${replacementMethodName}\` instead.`)
-			)
-			: (
-				`\`${deprecatedMethodName}\` is deprecated and will be removed in a future update.`
-			)
+	...args
+) => {
+	logDeprecation(
+		replacementMethodName
+		? (
+			`\`${deprecatedMethodName}\` is deprecated.`
+			.concat(' ')
+			.concat(`Use \`${replacementMethodName}\` instead.`)
 		)
+		: (
+			`\`${deprecatedMethodName}\` is deprecated and will be removed in a future update.`
+		)
+	)
 
-		return func(...args)
-	}
-)
+	return func(...args)
+}
 
 module.exports = createDeprecatedFunction
