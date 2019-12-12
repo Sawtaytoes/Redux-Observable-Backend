2d561c327421328d64f42dc95c2e24dde5d81bea
Kevin Ghadyani <Saturn2888@gmail.com>
2019-12-12 00:40:34 -0600

    Minor formatting in connectToServerEpic

--- packages/websocket/redux/externalConnections/connectToServerEpic.js
+++ packages/websocket/redux/externalConnections/connectToServerEpic.js
@@ -8,75 +8,83 @@ const { webSocket } = require('rxjs/webSocket')
 const { configurations } = require('@redux-observable-backend/node')
 
 const {
-	addServer,
-	CONNECT_TO_SERVER,
-	DISCONNECT_FROM_SERVER,
-	RECONNECT_TO_SERVER,
+  addServer,
+  CONNECT_TO_SERVER,
+  DISCONNECT_FROM_SERVER,
+  RECONNECT_TO_SERVER,
 } = require('./actions')
 
 const connectToServerEpic = (
-	action$,
-	state$,
+  action$,
+  state$,
 ) => (
-	action$
-	.pipe(
-		ofType(CONNECT_TO_SERVER),
-		mergeMap(({ namespace }) => (
-			of(state$.value)
-			.pipe(
-				map(
-					configurations
-					.selectors
-					.selectConfigurationSet({
-						namespace: 'externalConnections',
-					})
-				),
-				map(externalConnections => (
-					externalConnections[namespace]
-				)),
-				switchMap(({
-					hostname,
-					port,
-					protocol,
-					protocolVersion,
-				}) => (
-					action$
-					.pipe(
-						ofType(RECONNECT_TO_SERVER),
-						ofNamespace(namespace),
-						takeUntil(
-							action$
-							.pipe(
-								ofType(DISCONNECT_FROM_SERVER),
-								ofNamespace(namespace),
-							)
-						),
-						startWith(null),
-						map(() => (
-							webSocket({
-								protocol: protocolVersion,
-								url: (
-									protocol
-									.concat('://')
-									.concat(hostname)
-									.concat(':')
-									.concat(port)
-								),
-								WebSocketCtor: WebSocket,
-							})
-						)),
-						map(webSocketServer => (
-							addServer({
-								connection: webSocketServer,
-								namespace,
-							})
-						)),
-					)
-				)),
-			)
-		)),
-		catchEpicError(),
-	)
+  action$
+  .pipe(
+    ofType(CONNECT_TO_SERVER),
+    mergeMap(({
+      namespace: serverName,
+    }) => (
+      of(
+        state$
+        .value
+      )
+      .pipe(
+        map(
+          configurations
+          .selectors
+          .selectConfigurationSet({
+            namespace: 'externalConnections',
+          })
+        ),
+        map(externalConnections => (
+          externalConnections[serverName]
+        )),
+        switchMap(({
+          hostname,
+          port,
+          protocol,
+          protocolVersion,
+        }) => (
+          action$
+          .pipe(
+            ofType(RECONNECT_TO_SERVER),
+            ofNamespace(serverName),
+            takeUntil(
+              action$
+              .pipe(
+                ofType(
+                  RECONNECT_TO_SERVER,
+                  DISCONNECT_FROM_SERVER,
+                ),
+                ofNamespace(serverName),
+              )
+            ),
+            startWith(null),
+            map(() => (
+              webSocket({
+                protocol: protocolVersion,
+                url: (
+                  protocol
+                  .concat('://')
+                  .concat(hostname)
+                  .concat(':')
+                  .concat(port)
+                ),
+                WebSocketCtor: WebSocket,
+              })
+            )),
+            map(webSocketServer => (
+              addServer({
+                connection: webSocketServer,
+                namespace: serverName,
+              })
+            )),
+          )
+        )),
+      )
+    )),
+    catchEpicError(),
+  )
 )
 
 module.exports = connectToServerEpic
